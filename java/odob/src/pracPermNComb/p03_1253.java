63ff6b462af8baf40765315a136492a78a9ce875
Haneol <haneolj@gmail.com>
2025-03-07 00:46:54 +0900

    [Daily] 2025-03-06

---
+++ java/odob/src/pracPermNComb/p03_1253.java
@@ -0,0 +1,50 @@
+package pracPermNComb;
+
+import java.util.*;
+import java.io.*;
+
+public class p03_1253 {
+    static int[] numberList;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer numberListInputTokenizer = new StringTokenizer(bufferedReader.readLine());
+        numberList = new int[n];
+        for(int numberIndex = 0; numberIndex < n; numberIndex++) {
+            numberList[numberIndex] = Integer.parseInt(numberListInputTokenizer.nextToken());
+        }
+
+        // 음수도 되는구나 -> 혹시 시간초과 날 일 없나? 타입 int -> long 필요 없나?
+        // 정렬이 안되어있겠네
+        // n^2까지는 되겠다 일단..
+
+        // 투포인터 쓰면 되겠다
+        int left, right;
+        int countGoodNumber = 0;
+        for(int numberIndex = 0; numberIndex < n; numberIndex++) {
+            left = 0;
+            right = n - 1;
+
+            while(left < right) {
+                int sum = numberList[left] + numberList[right];
+
+                if(left == numberIndex) {
+                    left++;
+                } else if (right == numberIndex) {
+                    right--;
+                } else if(sum == numberList[numberIndex]) {
+                    countGoodNumber++;
+                    break;
+                } else if(sum < numberList[numberIndex]) { // 커져야 해 값이 -> 늘려야지(left만 늘릴 수 있어)
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        System.out.println(countGoodNumber);
+    }
+}
---
+++ java/odob/src/pracPermNComb/p04_1238.java
@@ -0,0 +1,106 @@
+package pracPermNComb;
+
+import java.util.*;
+import java.io.*;
+
+public class p04_1238 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // input
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 정점
+        int m = Integer.parseInt(st.nextToken()); // 간선
+        int x = Integer.parseInt(st.nextToken()); // 파티장소
+
+        // 그래프 문제가.. 만약에 이런식으로, 인접그래프가 나온다!
+        // 인덱스 = 정점
+        // 그 인덱스의 값은 = 이 정점과 연결된 다음 정점들의 리스트야.
+        // 1 : [2 3 4] -> 가변
+        // 2 : 1 3
+        // 3 : 1 4
+        // 4 : 2
+        // v
+        // 정적
+
+        List<Node>[] graph = new ArrayList[n + 1]; // 맞춰서 넣어주려고..
+        for(int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < m; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st2.nextToken());
+            int v2 = Integer.parseInt(st2.nextToken());
+            int time = Integer.parseInt(st2.nextToken());
+
+            graph[v1].add(new Node(v2, time));
+        }
+        // solve
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> {
+            return Integer.compare(a.time, b.time);
+        }); // time이 작은애가 우선순위가 높아
+        int[] dist = new int[n + 1]; // 방문체크이자, 업데이트 되는 시간(누적시간, a->b
+        int maxTime = 0;
+        for(int i = 1; i <= n; i++) {
+            Arrays.fill(dist, Integer.MAX_VALUE);
+
+            pq.offer(new Node(i, 0));
+            dist[i] = 0;
+            while(!pq.isEmpty()) {
+                Node cur = pq.poll();
+
+                // 왜? dist (최소) cur.time 이전에 계산한 누적시간
+                if (dist[cur.v] < cur.time) continue;
+
+                for(Node next : graph[cur.v]) { // a -> b, [k] a->b > a->k + k->b
+                    int time = dist[cur.v] + next.time;
+
+                    if(time < dist[next.v]) {
+                        dist[next.v] = time;
+                        pq.offer(new Node(next.v, time));
+                    }
+                }
+            }
+
+            int toParty = dist[x];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            pq.offer(new Node(x, 0));
+            dist[x] = 0;
+            while(!pq.isEmpty()) {
+                Node cur = pq.poll();
+
+                // 왜? dist (최소) cur.time 이전에 계산한 누적시간
+                if (dist[cur.v] < cur.time) continue;
+
+                for(Node next : graph[cur.v]) { // a -> b, [k] a->b > a->k + k->b
+                    int time = dist[cur.v] + next.time;
+
+                    if(time < dist[next.v]) {
+                        dist[next.v] = time;
+                        pq.offer(new Node(next.v, time));
+                    }
+                }
+            }
+
+            int fromParty = dist[i];
+
+            maxTime = Math.max(maxTime, toParty + fromParty);
+        }
+
+        // output
+        System.out.println(maxTime);
+
+        br.close();
+    }
+
+    static class Node {
+        int v;
+        int time;
+
+        Node(int v, int time) {
+            this.v = v;
+            this.time = time;
+        }
+    }
+}
